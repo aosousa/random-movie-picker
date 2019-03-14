05ac67bcbd08d5fc7b92a573f0c5201c98939d5d
André Sousa <andreosousa95@gmail.com>
2019-03-14 11:31:27 -0100

    OMDB integration through my movie-lookup package

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/aosousa/go-movielookup v0.0.0-20190305133634-37077fda0020
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/tealeg/xlsx v1.0.3
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
--- main.go
+++ main.go
@@ -1,42 +1,19 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/aosousa/go-movielookup/models"
 )
 
+var (
+	baseURL string
+	config  models.Config
+)
+
+func init() {
+	config = models.CreateConfig()
+	baseURL = "http://www.omdbapi.com/?apikey=" + config.APIKey + "&"
+}
+
 func main() {
-	var (
-		movie  string
-		movies [27][]string
-	)
-
-	XLSXLocation := `C:\Users\André\OneDrive\random\Movies.xlsx`
-
-	// pick random number to identify column (0 = A, 26 = AA)
-	column := random(0, 26)
-
-	// open XLSX document
-	xlsx, err := excelize.OpenFile(XLSXLocation)
-	handleError(err)
-
-	// get all rows in worksheet
-	rows := xlsx.GetRows("Movies")
-
-	// build movies slice
-	for _, row := range rows {
-		for i := 0; i < 27; i++ {
-			if row[i] != "" {
-				movies[i] = append(movies[i], row[i])
-			}
-		}
-	}
-
-	// pick random row
-	row := random(0, len(movies[column]))
-
-	// get movie from random column and row
-	movie = movies[column][row]
-	fmt.Printf("Movie: %s\n", movie)
+	pickRandomMovie()
 }
